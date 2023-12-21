c766fb5530c066f4cbdd2eebb63809ab828d386c
Umrii <aatiq145@gmail.com>
2023-12-21 10:07:37 +0500

    Validation and Dynamic Paths

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const sharp = require("sharp");
 const fs = require("fs");
 const sizeOf = require("image-size");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer();
 const path = require("path");
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
 const app = express();
+
+if (!process.env.UPLOAD_PATH || !process.env.IMAGE_DIRECTORY) {
+  throw new Error(
+    "Environment variables for paths are not properly configured."
+  );
+}
 
 // Function to resize and compress an image
 async function optimizeImage(
@@ -31,50 +39,53 @@ async function optimizeImage(
   }
 }
 // retrieve image height asynchronously
-async function getImageHeight(inputImagePath) {
+async function getImageHeight(inputImageBuffer) {
   try {
-    const image = await fs.promises.readFile(inputImagePath);
-    const dimensions = sizeOf(image);
+    const dimensions = sizeOf(inputImageBuffer);
     return dimensions.height;
   } catch (error) {
     throw new Error(`Error getting image height: ${error}`);
   }
 }
+app.use("/images", express.static(process.env.IMAGE_DIRECTORY));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  "/images",
-  express.static("C:\\Users\\aatiq\\OneDrive\\Desktop\\Optimization\\new")
-);
+const allowedImageTypes = ["image/jpeg", "image/png", "image/gif"]; // Add more as needed
+
+// Function to validate file type
+function validateFileType(file) {
+  return allowedImageTypes.includes(file.mimetype);
+}
 
 app.post("/upload-image", upload.single("image"), async (req, res) => {
   try {
-    const inputImagePath = req.file.path;
+    if (!req.file || !validateFileType(req.file)) {
+      throw new Error("Please upload a valid image file.");
+    }
+
+    const inputImageBuffer = req.file.buffer;
 
     const originalFilename = req.file.originalname;
     const extName = path.extname(originalFilename);
 
-    const outputDirectory =
-      "C:\\Users\\aatiq\\OneDrive\\Desktop\\Optimization\\new";
-
-    const outputImagePath = path.join(outputDirectory, originalFilename);
-
-    const height = await getImageHeight(inputImagePath);
-    const maxWidth = 600;
-    const maxHeight = Math.floor(height / 1.1);
-    const quality = 80;
-
-    await optimizeImage(
-      inputImagePath,
-      outputImagePath,
-      maxWidth,
-      maxHeight,
-      quality
-    );
+    const outputDirectory = process.env.IMAGE_DIRECTORY;
 
     const compressedFileName = `compressed_${Date.now()}${extName}`;
     const compressedImagePath = path.join(outputDirectory, compressedFileName);
 
-    fs.renameSync(outputImagePath, compressedImagePath);
+    const height = await getImageHeight(inputImageBuffer); // Pass buffer instead of file path
+    const maxWidth = 600;
+    const maxHeight = Math.floor(height / 1.1);
+    const quality = 80;
+
+    // Process the image from buffer and save the compressed version
+    await optimizeImage(
+      inputImageBuffer, // Pass buffer instead of file path
+      compressedImagePath,
+      maxWidth,
+      maxHeight,
+      quality
+    );
 
     const publicURL = `http://localhost:3000/images/${compressedFileName}`;
     res.status(200).json({
@@ -89,7 +100,6 @@ app.post("/upload-image", upload.single("image"), async (req, res) => {
     });
   }
 });
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
